b9d961be293791bbad1525277ed4c5cfaaa6d717
Gary <gzeri@uci.edu>
2018-07-03 15:05:18 -0700

    adds working code that create a newCoord file with only coords of atoms to be placed

--- moleculeMixer.py
+++ moleculeMixer.py
@@ -20,16 +20,45 @@ parser.add_argument("-d", "--distance", metavar="distance", type=float, default=
 parser.add_argument("-mxd", "--maxDistance", metavar="maxDistance", type=float, default=100)
 parser.add_argument("-mnd", "--minDistance", metavar="minDistance", type=float, default=0)
 parser.add_argument("-i", "--instances", metavar="instances", type=int, default=1)
+parser.add_argument("-f", "--fileType", metavar="fileType", type=str, default="tm")
 
 args = parser.parse_args()
 
 #open main coordinate file
 coord = open(args.coord,"r+")
 
-#open molecule to append file
+#open molecule to append to file
 molecule = open(args.molecule, "r")
 
 #Start function declarations###########################################
+def tmParser(line):
+    #prase turbomole file lines
+
+    #check for coord and end lines
+    if(line == "$coord\n" or line == "$end\n"):
+        return "NAN"
+
+    line2 = line.split("\n")[0].split(space)
+    print(line2)
+    
+    #check for stray spaces
+    if(len(line2) > 4):
+        line2.pop(0)
+
+    return line2
+
+#####################################################################
+def xyzParser(line):
+    #xyz parser
+
+    line2 = line.split("\n")[0].split("\r")[0].split(" ")
+    line2.append(line2[0])
+    line2.pop(0)
+    line2 = filter(str.strip,line2)
+
+    return line2
+
+#####################################################################
 def parseCoord(File):
     #given coord file, parses out distance and midpoint data 
     #and returns dictionary of data
@@ -39,27 +68,31 @@ def parseCoord(File):
             
             "midpoint":[0.0,0.0,0.0],
             "atomNumber": 0,
-            "file":File
+            "file":File,
+            "lineCount":0
 
             }
 
     #iterate over each line in the file that contains atom data
     for line in File:
 
-        #check for coord and end lines
-        if(line == "$coord\n" or line == "$end\n"):
+        #check file format to parse
+        if(args.fileType.lower() == "tm"):
+            line2 = tmParser(line)
+        elif(data["lineCount"] >= 2):
+            line2 = xyzParser(line)
+        else:
+            data["lineCount"] += 1
             continue
 
-        line2 = line.split("\n")[0].split(space)
+        if(line2 == "NAN"):
+            continue
+
         print(line2)
-        
+
         #increamnet atom count
         data["atomNumber"] += 1
 	
-	#check for stray spaces
-	if(len(line2) > 4):
-	    line2.pop(0)
-
         #increment midpoint
         for dim in range(3):
             data["midpoint"][dim] += float(line2[dim])
@@ -87,7 +120,11 @@ def RSI(coordData, moleculeData, args):
     Z = (hypotenuse * math.sin(theta) * math.cos(theta)) + coordData["midpoint"][2]
 
     newLines = newPosition(moleculeData, [X,Y,Z])
-    
+   
+    print("newlines")
+    print(newLines)
+    print("$$$$$$$$$$$$$$$$4")
+
     return newLines
 
 ############################################
@@ -96,23 +133,23 @@ def newPosition(data, newMidpoint):
     newLines = ""
     data["file"].seek(0)
 
+    lineCount = 0
+
     #calculate delta for each atom in molecule
     delta = [ newMidpoint[dim] - data["midpoint"][dim] for dim in range(3) ]
     print(0)
     #iterate overl all lines in coord file
     for line in data["file"]:
-        print(1)
-        #check for coord and end lines
-        if(line == "$coord\n" or line == "$end\n"):
+            
+        #check file format to parse
+        if(args.fileType.lower() == "tm"):
+            line2 = tmParser(line)
+        elif(lineCount >= 2):
+            line2 = xyzParser(line)
+        else:
+            lineCount += 1
             continue
-       
-        line2 = line.split("\n")[0].split(space)
-        print(line2[0])  
-       	
-	#check for stray spaces
-	if(len(line2) > 4):
-	    line2.pop(0)
-	
+
         #calculate new coord for each atom using delta
         newCoord = [ str(float(line2[dim]) + delta[dim]) for dim in range(3) ]
         
@@ -120,9 +157,14 @@ def newPosition(data, newMidpoint):
         print("newCoord")
 
         #convert newCoord to TM coord friendly data
-        newLines += "\n" + space + newCoord[0] + space + newCoord[1] + space + newCoord[2] + space + line2[3]
-        
-    return newLines[2:]
+        if(args.fileType.lower() == "tm"):
+            newLines += "\n" + space + newCoord[0] + space + newCoord[1] + space + newCoord[2] + space + line2[3]
+        else:
+            newLines += "\n" + line2[3] + space + newCoord[0] + space + newCoord[1] + space + newCoord[1]
+    print("testtestmnewlines")
+    print(newLines[:1])
+    print("!!!!!!!!!!!!!!!!")
+    return newLines
 
 #Start of main code###########################################
 #get coord and molecule data
@@ -139,15 +181,20 @@ linesToAppend = ""
 #uses random spherical integration method
 for i in range(args.instances):
     linesToAppend += RSI(coordData, moleculeData, args)
-    linesToAppend += "\n"
 
 #append new lines to new coord file
 #delete last line in coord file
-os.popen("head -n-1 " + args.coord + " > " + "new" + args.coord)
+#os.popen("head -n-1 " + args.coord + " > " + "new" + args.coord)
 
-#append new lines to file
-os.popen('echo "' + linesToAppend[:-1] + '" >> new' + args.coord)
-os.popen("echo '$end\n' >> new" + args.coord)
+print(linesToAppend)
+print("LINESTOAPPEND")
+
+#create new coord file with two molecules "mixed" together
+#createNewCoord(linesToAppend)
+
+#append new lines to new coord file
+os.popen('echo "' + linesToAppend + '" >> new' + args.coord)
+#os.popen("echo '$end\n' >> new" + args.coord)
 
 #close open files
 coordData["file"].close()
