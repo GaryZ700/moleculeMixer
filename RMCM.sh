a9af4f1c26af1a75968ef8de396f97a93b7dea5a
Gary <gzeri@uci.edu>
2018-07-03 20:10:58 -0700

    adds in working molecule mixer and management script

---
+++ RMCM.sh
@@ -0,0 +1,14 @@
+#system to manage massive runs of the molecular mixer to aid in Quinoline Analysis
+
+
+for i in {1..10}
+do
+	#run molecule mixer for specifed amount of times with specifed settings
+	python moleculeMixer.py -mxd 4 -c bq.xyz -c2 baseQuinoline.xyz -i 2 -m water.xyz -f xyz
+
+	#mv over new coord file to numbered configuration file
+	mv newbaseQuinoline.xyz q$i.xyz
+
+
+done
+
